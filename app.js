2fb31484e76c0a3a7a31396c599d0468fde5943a
b1shal711 <ba070101@gmail.com>
2022-11-28 16:16:19 +0530

    add procfile , add gitignore , update port

--- app.js
+++ app.js
@@ -78,7 +78,11 @@ app.get("/posts/:postId" , function(req,res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
